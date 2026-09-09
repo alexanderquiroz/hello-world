19d4dfe4568b51ca5d1d36e86cb0b103dc98fd6a
Alexander Quiroz <92895359+alexanderquiroz@users.noreply.github.com>
2026-09-09 10:11:42 -0500

    Add Supabase frontend config

---
+++ config.js
@@ -0,0 +1,5 @@
+window.DGIC_CONFIG = {
+  SUPABASE_URL: "https://ecuavthlvxarwrsayocl.supabase.co",
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_s0sduMXrcUqasIYqoLfWgA_AK7E5_J0",
+  PAGE_SIZE: 50
+};
